6b379a0bceec742495452a3cf0937de51860ebbf
Anezza <anezkagaberova@gmail.com>
2026-02-17 22:23:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,21 +9,22 @@ class Character:        #vytvoření class pro postavu
             print("game over")
         if collision with enemy_hitbox:        #odečtení poškození od životů
             self.hp = self.hp - dmg
-        elif collision with heal:
+        elif collision with heal:        #přičtení životů při léčení
             if self.hp == max_hp:
                 self.hp = self.hp
             else:
                 self.hp = self.hp + heal        #přičtení životů
-        else:
+        else:        #když se nic nestalo
             return self.hp
 
     def damage(self,buff,debuff):       #výpočet poškození postavy podle posílení nebo zeslabení
         if Character has buff:
-            self.dmg = self.dmg * buff
+            self.dmg = self.dmg * buff        
         elif Character has debuff:
             self.dmg = self.dmg * debuff
-        else:
+        else:        #když se nic nestalo
             return self.dmg
 
 #už daná postava
+
 postava = Character(20,3)
