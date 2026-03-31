0cdfe6126d79b8479beb860c237490ae3b1634f6
Anezza <anezkagaberova@gmail.com>
2026-04-01 00:11:36 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 import pygame
 pygame.init()
-
-IMAGE_PATH = 'pics/st.png'
-BUTTON_IMAGE_PATH = 'pics/button.png'
-
+#Cesty k obrázkům
+IMAGE_PATH = 'images/st.png'
+BUTTON_IMAGE_PATH = 'images/button.png'
+#Nastavení rozměrů
 WIDTH = 1280
 HEIGHT = 720
-
+#Nastavení postavy
 PLAYER_SPEED = 1
 PLAYER_WIDTH = 100
 PLAYER_HEIGHT = 100
@@ -15,9 +15,10 @@ PLAYER_HEIGHT = 100
 
 
 
-
+#Definice fontů
 title_font = pygame.font.SysFont('Arial',100)
 menu_font = pygame.font.SysFont('Arial',50)
+#Hlavní menu - Texty a jejich umístění
 title_text = title_font.render("Accursed souls", True, (255,255,255))
 title_rect = title_text.get_rect(midleft=(70,70))
 play_text = menu_font.render("Play", True, (255,255,255))
@@ -26,7 +27,7 @@ settings_text = menu_font.render("Settings", True, (255,255,255))
 settings_rect = settings_text.get_rect(midleft=(70,300))
 exit_text = menu_font.render("Exit", True, (255,255,255))
 exit_rect = exit_text.get_rect(midleft=(70,600))
-
+#Menu Nastavení
 audio_text = menu_font.render("Audio", True, (255,255,255))
 audio_rect = audio_text.get_rect(midleft=(70,200))
 video_text = menu_font.render("Video", True, (255,255,255))
@@ -35,7 +36,7 @@ gameplay_text = menu_font.render("Gameplay", True, (255,255,255))
 gameplay_rect = gameplay_text.get_rect(midleft=(70,400))
 return_text = menu_font.render("return", True, (255,255,255))
 return_rect = return_text.get_rect(midleft=(70,600))
-
+#Podrobnosti v nastavení
 difficulty_text = menu_font.render("Difficulty", True, (255,255,255))
 difficulty_rect = difficulty_text.get_rect(center=(440,200))
 
