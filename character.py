0ccfa8348f53f8139ce76de47c40a0cdf914d9e9
Anezza <anezkagaberova@gmail.com>
2026-04-01 00:07:18 +0200

    Update character.py

--- character.py
+++ character.py
@@ -3,13 +3,15 @@ from settings import *
 class Character(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        #Načtení a příprava obrázku postavy
         self.image = pygame.image.load(IMAGE_PATH).convert_alpha()
         self.image = pygame.transform.scale(self.image,(PLAYER_WIDTH,PLAYER_HEIGHT))
+        #Nastavení pozice postavy na obrazovce
         self.rect = self.image.get_rect(center = (WIDTH//2,HEIGHT-30))
         self.speed = PLAYER_SPEED
     def update(self):
         self.rect.y += self.speed*1.5
-        keys = pygame.key.get_pressed()
+        keys = pygame.key.get_pressed()    #Ovládání postavy pomocí klávesnice
         if keys[pygame.K_a] and self.rect.left > 0:
             self.rect.x -= self.speed
         if keys[pygame.K_d] and self.rect.right < WIDTH:
